7f994d2c3bb3758b8bbd23a4348a50755b11436e
s2cl <yukisunictf@gmail.com>
2018-11-24 22:46:11 +0900

    added judge script

--- Assets/Scripts/Notes.cs
+++ Assets/Scripts/Notes.cs
@@ -16,6 +16,38 @@ public class Notes : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		this.transform.position = new Vector3(x, y-Time.time*5);
+		this.transform.position = new Vector3(x, y-Time.time*5 -4);
+		float judgetime = y-Time.time*5;
+
+		// 判定後だったら
+		if (judgetime < -2){
+			Debug.Log("poor");
+			Destroy(this.gameObject);
+		}
+		// 判定前だったら
+		else if (judgetime > 0.15){
+		}
+
+		else if (Input.GetKeyDown("s") || Input.GetKeyDown("l")){
+			// perfect
+			if (judgetime <= 0.02 && judgetime >= -0.02 ){
+				Debug.Log("Perfect!");
+			}
+			else if (judgetime <= 0.04 && judgetime >= -0.04 ){
+				Debug.Log("Great");
+			}
+			else if (judgetime <= 0.105 && judgetime >= -0.105 ){
+				Debug.Log("good");
+			}
+			else if (judgetime <= 0.150 && judgetime >= -0.150 ){
+				Debug.Log("bad...");
+			}
+			else{
+				Debug.Log("poor");
+			}
+			Destroy(this.gameObject);
+
+		}
+
 	}
 }
